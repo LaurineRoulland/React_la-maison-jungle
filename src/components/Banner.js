5af784ee0cd6fc851878a7e9c4b2a0d3d6d7f5e0
LaurineRoulland <l.roulland@hotmail.fr>
2023-05-02 12:11:01 +0200

    Update component Banner (props/children)

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,5 @@
-import logo from '../assets/logo.png'
 import '../styles/banner.css'
 
-export const Banner = () => {
-    return (
-        <div className="lmj-banner">
-            <img src={logo} alt="Logo de la maison jungle" className="lmj-logo" />
-            <h1> La maison Jungle </h1>
-        </div>
-    )
+export const Banner = ({ children }) => {
+    return <div className="lmj-banner">{children}</div>
 }
