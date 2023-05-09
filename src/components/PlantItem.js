69bf2e764388ce137b40b9d00f6452e90e546814
LaurineRoulland <l.roulland@hotmail.fr>
2023-05-09 15:06:01 +0200

    Create component Footer + css

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -2,8 +2,17 @@ import { CareScale } from './CareScale'
 import '../styles/plantItem.css'
 
 export const PlantItem = ({ name, cover, id, light, water }) => {
+
+    // const handleClick = (plantName) => {
+    //     alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix ✨`)
+    // }
+    function handleClick(e) {
+        console.log(`Ceci est mon event :`, e)
+    }
+
     return (
-        <li key={id} className="lmj-plant-item">
+
+        <li key={id} className="lmj-plant-item" onClick={() => handleClick(name)}>
             <img className="lmj-plant-item-cover" src={cover} alt={`${name} cover`} />
             {name}
             <div>
