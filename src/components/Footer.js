f3564877025725943f3ddc844e010d603da21856
LaurineRoulland <l.roulland@hotmail.fr>
2023-05-09 16:44:01 +0200

    Update Footer.js
    Création d'un formulaire + bouton pour entrer une adresse email valide

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,35 @@ import '../styles/footer.css'
 export const Footer = () => {
     const [inputValue, setInputValue] = useState('')
 
+    function Input(event) {
+        setInputValue(event.target.value)
+    }
+
+    function isNotEmail() {
+        if (!inputValue.includes('@')) {
+            alert("Sans @, ce n'est pas une adresse mail !")
+        }
+    }
+
     return (
         <footer className="lmj-footer">
             <div className="lmj-footer-elem">
                 Pour les passionné·e·s de plantes 🌿🌱🌵
             </div>
-            <div className="lmj-footer-elem">Laissez-nous votre mail :</div>
+            <div className="lmj-footer-elem">
+                <p>Laissez-nous votre mail :</p>
+                <form className="lmj-footer-form">
+                    <label>
+                        <input
+                            value={inputValue}
+                            onChange={Input}
+                            placeholder="Email"
+                            onBlur={isNotEmail}
+                        />
+                        <button type="submit">Entrer</button>
+                    </label>
+                </form>
+            </div>
         </footer>
     )
 }
