e51ab072395c2d33706d003bd4905aa3e3f4f812
LaurineRoulland <l.roulland@hotmail.fr>
2023-05-09 10:36:37 +0200

    Update PlantItem.js

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,1 +1,15 @@
-//export const plantItem = () => {}
+import { CareScale } from './CareScale'
+import '../styles/plantItem.css'
+
+export const PlantItem = ({ name, cover, id, light, water }) => {
+    return (
+        <li key={id} className="lmj-plant-item">
+            <img className="lmj-plant-item-cover" src={cover} alt={`${name} cover`} />
+            {name}
+            <div>
+                <CareScale careType="water" scaleValue={water} />
+                <CareScale careType="light" scaleValue={light} />
+            </div>
+        </li>
+    )
+}
