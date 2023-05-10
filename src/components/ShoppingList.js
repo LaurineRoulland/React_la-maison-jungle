cab21e8f3d8a655b3ff897f1b16c7b4b5170d391
LaurineRoulland <l.roulland@hotmail.fr>
2023-05-10 12:38:43 +0200

    Update ShoppingList.js
    restructuration de la fonction permettant l'ajout, la suppression + affichage des propriétés des plantes

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,7 +2,7 @@ import '../styles/shoppingList.css'
 import { plantList } from '../datas/plantList'
 import { PlantItem } from './PlantItem'
 
-export const ShoppingList = () => {
+export const ShoppingList = ({ cart, updateCart }) => {
     const categories = plantList.reduce(
         (accumulator, plant) =>
             accumulator.includes(plant.category)
@@ -11,22 +11,39 @@ export const ShoppingList = () => {
         []
     )
 
+    function addToCart(name, price) {
+        const currentPlantSaved = cart.find((plant) => plant.name === name)
+        if (currentPlantSaved) {
+            const cartFilteredCurrentPlant = cart.filter((plant) => plant.name !== name)
+            updateCart([
+                ...cartFilteredCurrentPlant,
+                { name, price, amount: currentPlantSaved.amount + 1 },
+            ])
+        } else {
+            updateCart([...cart, { name, price, amount: 1 }])
+        }
+    }
+
     return (
-        <div>
+        <div className="lmj-shopping-list">
             <ul>
                 {categories.map((category) => (
                     <li key={category}> {category} </li>
                 ))}
             </ul>
             <ul className="lmj-plant-list">
-                {plantList.map(({ id, cover, name, water, light }) => (
-                    <PlantItem
-                        id={id}
-                        cover={cover}
-                        name={name}
-                        water={water}
-                        light={light}
-                    />
+                {plantList.map(({ id, cover, name, water, light, price }) => (
+                    <div key={id}>
+                        <PlantItem
+                            id={id}
+                            cover={cover}
+                            name={name}
+                            water={water}
+                            light={light}
+                            price={price}
+                        />
+                        <button onClick={() => addToCart(name, price)}>Ajouter</button>
+                    </div>
                 ))}
             </ul>
         </div>
